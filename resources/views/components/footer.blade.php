d0ec5943f715293a33bf780c1d7ff040089c3dce
julien <zigan.julien@gmail.com>
2025-12-17 20:06:39 +0100

    ui: add landing page

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,47 @@
+<footer class="bg-dark text-white pt-5 pb-4 mt-auto">
+    <div class="container text-center text-md-start">
+        <div class="row text-center text-md-start">
+
+            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                <h5 class="text-uppercase mb-4 font-weight-bold text-primary">JULIENS LARAVEL</h5>
+                <p>Auf dieser Seite können Sie nachvollziehen, welche Fortschritte ich in meinen PHP- und Laravel-Kenntnissen mache.</p>
+            </div>
+
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h5 class="text-uppercase mb-4 font-weight-bold text-primary">Nützliche Links</h5>
+                <p><a href="/" class="text-white text-decoration-none">Home</a></p>
+                <p><a href="/about" class="text-white text-decoration-none">Über mich</a></p>
+                <p><a href="/contact" class="text-white text-decoration-none">Kontakt</a></p>
+                <p><a href="/terms" class="text-white text-decoration-none">AGB</a></p>
+            </div>
+
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                <h5 class="text-uppercase mb-4 font-weight-bold text-primary">Kontakt</h5>
+                <p><i class="bi bi-house-door-fill me-2"></i> Berlin, DE</p>
+                <p><i class="bi bi-envelope-fill me-2"></i> info@example.com</p>
+                <p><i class="bi bi-telephone-fill me-2"></i> + 01 234 567 89</p>
+            </div>
+        </div>
+
+        <hr class="mb-4">
+
+        <div class="row align-items-center">
+            <div class="col-md-7 col-lg-8">
+                <p>Copyright © {{ date('Y') }} All rights reserved by:
+                    <a href="#" class="text-primary text-decoration-none">
+                        <strong>JULIENS LARAVEL</strong>
+                    </a>
+                </p>
+            </div>
+
+            <div class="col-md-5 col-lg-4">
+                <div class="text-center text-md-end">
+                    <a href="#" class="btn btn-outline-light btn-sm rounded-circle me-2"><i class="bi bi-facebook"></i></a>
+                    <a href="#" class="btn btn-outline-light btn-sm rounded-circle me-2"><i class="bi bi-twitter"></i></a>
+                    <a href="#" class="btn btn-outline-light btn-sm rounded-circle me-2"><i class="bi bi-google"></i></a>
+                    <a href="#" class="btn btn-outline-light btn-sm rounded-circle"><i class="bi bi-github"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
