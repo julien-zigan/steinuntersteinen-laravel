8213d582725fc69c5381cdb11473a3ab0839695f
julien <zigan.julien@gmail.com>
2025-12-18 08:23:39 +0100

    routing: add home controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('pages.welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
